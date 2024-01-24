d56aaf5c5b7c8f76b950e72bb97d4f46795158d9
Aleksandre Maisuradze <aleksandremaisuradze11@gmail.com>
2024-01-24 21:56:43 +0400

    finish calculator

--- script/script.js
+++ script/script.js
@@ -84,3 +84,22 @@ function restartGame() {
 }
 
 //Calculator
+
+let input = document.querySelector('.main_input')
+
+function addToInput(value) {
+    input.value += value;
+}
+
+function clearInput() {
+    input.value = ''
+}
+
+function calculate() {
+    try {
+        input.value = eval(input.value)
+    }
+    catch (error) {
+        input.value = 'error'
+    }
+}
