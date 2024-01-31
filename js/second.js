3a3a7847e136020972d71f86736b9ca33619318e
Aleksandre Maisuradze <aleksandremaisuradze11@gmail.com>
2024-01-31 14:17:01 +0400

    last changes

--- js/second.js
+++ js/second.js
@@ -74,3 +74,59 @@ function inputInterval(func) {
         }, 700)
     }
 }
+
+
+
+//slider
+
+
+let slider = document.querySelector('.background_section');
+let sliderImages = slider.children[0].children;
+let arrowButtons = slider.children[1].children;
+
+let activeSlider = 0;
+let prevSlider = 0;
+
+sliderImages[activeSlider].classList.add('active1')
+
+function classChanger() {
+    sliderImages[activeSlider].classList.add('active1');
+    sliderImages[prevSlider].classList.remove('active1');
+}
+
+function next() {
+    if (activeSlider < sliderImages.length - 1) {
+        activeSlider++;
+        prevSlider = activeSlider - 1;
+        classChanger();
+    } else {
+        activeSlider = 0;
+        prevSlider = sliderImages.length - 1;
+        classChanger();
+    }
+}
+
+
+function prev() {
+    if (activeSlider > 0) {
+        activeSlider--;
+        prevSlider = activeSlider + 1;
+        classChanger();
+    } else {
+        activeSlider = sliderImages.length - 1;
+        prevSlider = 0;
+        classChanger();
+    }
+}
+
+arrowButtons[0].addEventListener('click', prev);
+arrowButtons[1].addEventListener('click', next);
+
+
+function autoChange(delay = 5000) {
+    setInterval(() => {
+        next()
+    }, delay)
+}
+
+autoChange()
